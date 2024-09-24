5f8fb6a742b81f965237a07aef627b9faf1d177c
USER <onyekaejimns@gmail.com>
2024-09-24 13:14:33 +0100

    datatypes

---
+++ dataInputsOperators.py
@@ -0,0 +1,24 @@
+a=20
+b=40
+# + addition
+print(a+b)
+# / division
+print(a/b)
+# // rpunding off division
+print(a//b)
+# < lessthan >greater than
+print(a<b)
+print(a>b)
+# ** raise to power
+num1=2
+print(num1**3)
+# % remainder
+print(a%b)
+# or and not
+print(a<1or a!=1)
+print(a>1 and a!=1)
+print(not a>1 and a!=1)
+# inputs
+input1=input("enter a : ")
+input2=input("enter b : ")
+print(input1+input2)
---
+++ variables.py
@@ -0,0 +1,17 @@
+# declaration of variables in python
+a=20
+b=20.30
+c="characters"
+d="400"
+areYouHappy=True
+print(a)
+print(b)
+print(c)
+# variable types it is used to know the datatype of a variable or value
+print(type(a))
+print(type(areYouHappy))
+# datatype conversion a conversion from one datatype to another in pythgon
+e=int(d)+a
+print(e)
+name=c+str(a)
+print(name)
